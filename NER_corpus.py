2e23c7ba6da8e88dedc6603d1e328d46d8d2b81c
Akangkshya Pathak <akangkshyap@gmail.com>
2021-09-30 04:47:02 +0530

    Update NER_corpus.py

--- NER_corpus.py
+++ NER_corpus.py
@@ -1,11 +1,11 @@
 #Training data
 
-with open("E://7th sem CSE/internship/CRF_4_NER-master/train.txt", "r") as f:
+with open("Directory/train.txt", "r") as f:
     sents = [line.strip() for line in f.readlines()]
 
 
 train_num = int((len(sents)//3))
-with open("E://7th sem CSE/internship/CRF_4_NER-master/train.data", "w") as g:
+with open("Directory/train.data", "w") as g:
     for i in range(train_num):
         words = sents[3*i].split('\t')
         postags = sents[3*i+1].split('\t')
@@ -22,12 +22,12 @@ print('OK train!')
 
 #Testing Data
 
-with open("E://7th sem CSE/internship/CRF_4_NER-master/test.txt", "r") as f:
+with open("Directory/test.txt", "r") as f:
     sents = [line.strip() for line in f.readlines()]
 
 
 test_num = int((len(sents)//3))
-with open("E://7th sem CSE/internship/CRF_4_NER-master/test.data", "w") as g:
+with open("Directory/test.data", "w") as g:
     for i in range(test_num):
         words = sents[3*i].split('\t')
         postags = sents[3*i+1].split('\t')
